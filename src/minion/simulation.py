1052adcd30d3e2ebbc4c8a46df33b4b09c7307d9
paucosta04-beep <pau.costa04@estudiant.upf.edu>
2025-11-14 03:01:01 +0100

    feat(simulation): add random hi/bye behavior to say_hi

--- src/minion/simulation.py
+++ src/minion/simulation.py
@@ -1,2 +1,8 @@
-def say_hi(name="bob"):
-    print("hello", name)
+import random
+
+def say_hi(name="caracol"):
+    coin = random.random() > 0.5
+    if coin:
+        print("hi", name)
+    else:
+        print("bye", name)
